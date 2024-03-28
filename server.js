eaa76b744833b658c6620307e6ad04ca84ea142e
Chiranthan Y <mechiranthan.y@gmail.com>
2024-03-28 21:00:34 +0530

    added eslint

--- server.js
+++ server.js
@@ -1,40 +1,41 @@
-const express = require("express");
-const http = require("http");
-const socketIo = require("socket.io");
-const mongoose = require("mongoose");
+require('dotenv').config()
+const express = require('express')
+const http = require('http')
+const socketIo = require('socket.io')
+const mongoose = require('mongoose')
 
-const app = express();
-const server = http.createServer(app);
-const io = socketIo(server);
+const app = express()
+const server = http.createServer(app)
+const io = socketIo(server)
 
 // MongoDB setup
-mongoose.connect("mongodb://localhost:27017/chatAppDB", {
+mongoose.connect('mongodb://localhost:27017/chatAppDB', {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "MongoDB connection error:"));
-db.once("open", () => {
-  console.log("Connected to MongoDB");
-});
+  useUnifiedTopology: true
+})
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+db.once('open', () => {
+  console.log('Connected to MongoDB')
+})
 
 // Socket.IO setup
-io.on("connection", (socket) => {
-  console.log("a user connected");
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
-  });
-});
+io.on('connection', (socket) => {
+  console.log('a user connected')
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
+  })
+})
 
 // middleware
 
 // Express routes
-app.get("/", (req, res) => {
-  res.send("Chat App API");
-});
+app.get('/', (req, res) => {
+  res.send('Chat App API')
+})
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+  console.log(`Server is running on port ${PORT}`)
+})
